bbeb92805c30e3f64e39d900a18f0e2d68ab8732
LeeKennedy <mail@leekennedy.id.au>
2017-09-07 20:25:31 +1000

    Dropbox code

---
+++ Miscellaneous/dropbox.R
@@ -0,0 +1,18 @@
+if (Sys.info()['sysname'] == 'Darwin') {
+        info <- RJSONIO::fromJSON(
+                file.path(path.expand("~"),'.dropbox','info.json'))
+}
+if (Sys.info()['sysname'] == 'Windows') {
+        info <- RJSONIO::fromJSON(
+                if (file.exists(file.path(Sys.getenv('APPDATA'), 'Dropbox','info.json'))) {
+                        file.path(Sys.getenv('APPDATA'), 'Dropbox', 'info.json')
+                } else {
+                        file.path(Sys.getenv('LOCALAPPDATA'),'Dropbox','info.json')
+                }
+        )
+}
+
+
+dropbox_base <- info$personal$path
+
+
