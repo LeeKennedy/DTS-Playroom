bdebbee9fb2f8880f230b115132d2d921382b800
LeeKennedy <mail@leekennedy.id.au>
2019-04-26 12:27:30 +1000

    Scripts to produce anonymous IDs for clients other than the ones of interest in a data set.

---
+++ Anonymous Samples/anon_customer.R
@@ -0,0 +1,59 @@
+#### Clean Up environment -----------------------------
+rm(list=ls())
+
+#### Packages -----------------------------
+library(readxl)
+library(tidyverse)
+library(here)
+
+
+#### Functions -----------------------------
+outliers <- function (x, b = FALSE) {
+xx <- sapply(x, as.numeric)
+
+#xx <- sort(xx)
+
+remove_outliers <- function(x, na.rm = TRUE, ...) {
+  qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
+  H <- 1.5 * IQR(x, na.rm = na.rm)
+  y <- x
+  y[x < (qnt[1] - H)] <- NA
+  y[x > (qnt[2] + H)] <- NA
+  y
+}
+
+yy <- remove_outliers(xx)
+ww <- remove_outliers(yy)
+zz <- remove_outliers(ww)
+
+diff.out <- data.frame(xx, yy, ww, zz)
+
+if(b == TRUE){
+boxplot(diff.out)
+}
+
+return(zz)
+}
+
+#### Data Input -----------------------------
+here()
+
+data <- read_excel("PVAL05.xlsx", col_types = c("numeric", 
+                                                "date", "date", "text", "text", "text", 
+                                                "text", "numeric", "text", "text", "text", 
+                                                "text", "text", "text", "text", "text", 
+                                                "text", "text", "numeric"))
+
+target_customers <- as.list(c("MG80", "MG70"))
+
+#### Data Working -----------------------------
+
+data <- data %>% 
+        mutate(ID = group_indices(., CUSTOMER)) 
+
+j <- nrow(data)
+
+for(i in 1:j){
+        if(data$CUSTOMER[i] %in% target_customers == TRUE)
+                data$ID[i] <- data$CUSTOMER[i]
+}
