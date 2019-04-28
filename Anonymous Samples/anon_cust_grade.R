e770255530cb0478b24abf06fb1b613f70c6cbd9
LeeKennedy <mail@leekennedy.id.au>
2019-04-28 13:55:14 +1000

    Reduced most of scripts to functions

--- Anonymous Samples/anon_cust_grade.R
+++ Anonymous Samples/anon_cust_grade.R
@@ -9,6 +9,40 @@ library(here)
 
 #### Functions -----------------------------
 
+anon_client_grade <- function(a,b){
+        A1 <- LETTERS
+        A2 <- LETTERS
+        
+        label_list <- expand.grid(A2, A1)
+        label_list$n <- as.numeric(rownames(label_list))
+        label_list$ID <- paste(label_list$Var2, label_list$Var1, sep="")
+        label_list <- label_list[,c(3,4)]
+        
+        a <- a %>% 
+                mutate(cust_id = group_indices(., CUSTOMER), 
+                       grade_id = group_indices(., PRODUCT_GRADE)) 
+        
+        a$prime_ID <- paste(a$CUSTOMER, a$PRODUCT_GRADE, sep = "_")
+        
+        a <- merge(a, label_list, by.x = "cust_id", by.y = "n", all.x = TRUE)
+        colnames(a)[23] <- "Cust_Code"
+        
+        a <- merge(a, label_list, by.x = "grade_id", by.y = "n", all.x = TRUE)
+        colnames(a)[24] <- "Grade_Code"
+        
+        a$ID <- paste(a$Cust_Code, a$Grade_Code, sep = "")
+        a <- a[,c(3:22,25)]
+        
+        j <- nrow(a)
+        
+        for(i in 1:j){
+                if(a$CUSTOMER[i] %in% b == TRUE)
+                        a$ID[i] <- a$prime_ID[i]
+        }
+        return (a)
+        
+}
+
 #### Data Input -----------------------------
 here()
 
@@ -22,34 +56,9 @@ data <- read_excel("Anonymous Samples/PVAL05.xlsx",
 
 target_customers <- as.list(c("MG80", "MG70"))
 
-#### Data Working -----------------------------
+#### Run function --------------------------
+#### Function will take the CUSTOMER list and PRODUCT_GRADE list and remove the identity of all clients information
+#### other than those in the target customer list.
 
-A1 <- LETTERS
-A2 <- LETTERS
 
-label_list <- expand.grid(A2, A1)
-label_list$n <- as.numeric(rownames(label_list))
-label_list$ID <- paste(label_list$Var2, label_list$Var1, sep="")
-label_list <- label_list[,c(3,4)]
-
-data <- data %>% 
-        mutate(cust_id = group_indices(., CUSTOMER), 
-               grade_id = group_indices(., PRODUCT_GRADE)) 
-
-data$prime_ID <- paste(data$CUSTOMER, data$PRODUCT_GRADE, sep = "_")
-
-data <- merge(data, label_list, by.x = "cust_id", by.y = "n", all.x = TRUE)
-colnames(data)[23] <- "Cust_Code"
-
-data <- merge(data, label_list, by.x = "grade_id", by.y = "n", all.x = TRUE)
-colnames(data)[24] <- "Grade_Code"
-
-data$ID <- paste(data$Cust_Code, data$Grade_Code, sep = "")
-data <- data[,c(3:22,25)]
-
-j <- nrow(data)
-
-for(i in 1:j){
-        if(data$CUSTOMER[i] %in% target_customers == TRUE)
-                data$ID[i] <- data$prime_ID[i]
-}
+temp <- anon_client_grade(data, target_customers)
--- Anonymous Samples/anon_customer.R
+++ Anonymous Samples/anon_customer.R
@@ -9,8 +9,35 @@ library(here)
 
 #### Functions -----------------------------
 
+anonymous <- function(a,b) {
+        A1 <- LETTERS
+        A2 <- LETTERS
+        
+        label_cust <- expand.grid(A2, A1)
+        label_cust$n <- as.numeric(rownames(label_cust))
+        label_cust$ID <- paste(label_cust$Var2, label_cust$Var1, sep="")
+        label_cust <- label_cust[,c(3,4)]
+        
+        #### Data Working -----------------------------
+        
+        a <- a %>% 
+                mutate(ID2 = group_indices(., CUSTOMER)) 
+        
+        a <- merge(a, label_cust, by.x = "ID2", by.y = "n", all.x = TRUE)
+        a <- a[,-1]
+        
+        j <- nrow(data)
+        
+        for(i in 1:j){
+                if(a$CUSTOMER[i] %in% b == TRUE)
+                        a$ID[i] <- a$CUSTOMER[i]
+                
+        }   
+        return (a)
+}
 
 #### Data Input -----------------------------
+
 here()
 
 data <- read_excel("Anonymous Samples/PVAL05.xlsx", 
@@ -22,25 +49,8 @@ data <- read_excel("Anonymous Samples/PVAL05.xlsx",
 
 target_customers <- as.list(c("MG80", "MG70"))
 
-A1 <- LETTERS
-A2 <- LETTERS
+#### Run function --------------------------
+#### Function will take the CUSTOMER list and remove the identity of all clients 
+#### other than those in the target customer list.
 
-label_cust <- expand.grid(A2, A1)
-label_cust$n <- as.numeric(rownames(label_cust))
-label_cust$ID <- paste(label_cust$Var2, label_cust$Var1, sep="")
-label_cust <- label_cust[,c(3,4)]
-
-#### Data Working -----------------------------
-
-data <- data %>% 
-        mutate(ID2 = group_indices(., CUSTOMER)) 
-
-data <- merge(data, label_cust, by.x = "ID2", by.y = "n", all.x = TRUE)
-data <- data[,-1]
-
-j <- nrow(data)
-
-for(i in 1:j){
-        if(data$CUSTOMER[i] %in% target_customers == TRUE)
-                data$ID[i] <- data$CUSTOMER[i]
-}
+temp <- anonymous(data, target_customers)
